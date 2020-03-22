d3782fd458e6af6dfcf71e2b2fde3ca616a34052
Mika <mika@wappia.se>
2020-03-22 09:54:38 +0800

    Add dev mode, using HEAD

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import config from './smartes.js'
 import App from './lib/App.js'
+
+let config
+
+try {
+  config = import(process.cwd() + '/smartes.js')
+} catch (e) {
+  console.error('Could not load "smartes.js" file')
+  process.exit(1)
+}
 
 App(config)
