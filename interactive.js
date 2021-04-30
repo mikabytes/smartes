368f0c8eae2ce5f72563a06ca0f6bcff47135253
Mika <mika@wappia.se>
2021-04-30 13:50:11 +0200

    Add missing import

--- interactive.js
+++ interactive.js
@@ -1,6 +1,29 @@
-import smartes from "./lib/builder.js"
-import fs from "fs/promises"
+import builder from "./lib/builder.js"
+import fs from "fs"
 
-const out = smartes({ entry: `lib/builder.js`, path: `.`, skipCache })
+async function run() {
+  const smartes = await builder({
+    entry: `lib/builder.js`,
+    path: `.`,
+    skipCache: true,
+  })
+  const out = await smartes(`master`)
+  console.log(out.lib.entries)
 
-fs.writeFileSync(`/tmp/out.json`, JSON.serialize(out, null, 4))
+  await expand(out.lib)
+
+  fs.writeFileSync(`/tmp/out.json`, JSON.stringify(out, null, 4))
+}
+
+run()
+
+async function expand(thing) {
+  if (thing && thing.contents) {
+    thing.a = await thing.contents()
+  }
+  if (thing.entries) {
+    await Promise.all(
+      Object.keys(thing.entries).map((it) => expand(thing.entries[it]))
+    )
+  }
+}
