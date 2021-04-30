c857f6e8b3ad795aff006c1252b1c340e4b19a9c
Mika <mika@wappia.se>
2021-04-30 16:05:54 +0200

    dependencyGraph to export information about static/dynamic

--- test/dependencyGraph.js
+++ test/dependencyGraph.js
@@ -1,6 +1,6 @@
 import assert from "assert"
 
-import DependencyGraph from "../src/dependencyGraph.js"
+import DependencyGraph from "../lib/dependencyGraph.js"
 
 describe("dependency graph", () => {
   let dg, schema
@@ -15,10 +15,12 @@ describe("dependency graph", () => {
       "index.js": {
         hash: "ide",
         version: 1,
+        isDynamic: false,
       },
       "two.js": {
         hash: "222",
         version: 1,
+        isDynamic: false,
       },
     })
   })
@@ -36,10 +38,12 @@ describe("dependency graph", () => {
       "index.js": {
         hash: "ide",
         version: 2,
+        isDynamic: false,
       },
       "two.js": {
         hash: "222-changed",
         version: 2,
+        isDynamic: false,
       },
     })
   })
@@ -52,10 +56,12 @@ describe("dependency graph", () => {
       "index.js": {
         hash: "1",
         version: 3,
+        isDynamic: false,
       },
       "two.js": {
         hash: "222-changed",
         version: 2,
+        isDynamic: false,
       },
     })
   })
@@ -69,22 +75,27 @@ describe("dependency graph", () => {
       "index.js": {
         hash: "11",
         version: 4,
+        isDynamic: false,
       },
       "two.js": {
         hash: "222-changed",
         version: 2,
+        isDynamic: false,
       },
       "circular-1.js": {
         hash: "c1",
         version: 1,
+        isDynamic: false,
       },
       "circular-2.js": {
         hash: "c2",
         version: 1,
+        isDynamic: false,
       },
       "circular-3.js": {
         hash: "c3",
         version: 1,
+        isDynamic: false,
       },
     })
   })
@@ -99,22 +110,54 @@ describe("dependency graph", () => {
       "index.js": {
         hash: "11",
         version: 5, // note that this also gets version increase because it depends on a circular dependency
+        isDynamic: false,
       },
       "two.js": {
         hash: "222-changed",
         version: 2,
+        isDynamic: false,
       },
       "circular-1.js": {
         hash: "c1",
         version: 2,
+        isDynamic: false,
       },
       "circular-2.js": {
         hash: "c2",
         version: 2,
+        isDynamic: false,
       },
       "circular-3.js": {
         hash: "c3",
         version: 2,
+        isDynamic: false,
+      },
+    })
+  })
+
+  it("should always consider circular dependencies dirty", async () => {
+    schema = await dg.add(snapshots[4])
+
+    assert.deepEqual(schema, {
+      "index.js": {
+        hash: "11",
+        version: 2,
+        isDynamic: false,
+      },
+      "two.js": {
+        hash: "222",
+        version: 1,
+        isDynamic: false,
+      },
+      dynamic1: {
+        hash: "dead beef",
+        isDynamic: true,
+        version: 1,
+      },
+      "static-from-dynamic": {
+        hash: "beef",
+        isDynamic: true,
+        version: 1,
       },
     })
   })
@@ -171,6 +214,20 @@ const snapshots = [
     "circular-3.js": {
       hash: async () => "c3",
       contents: async () => `import './circular-1.js'`,
+    },
+  },
+  {
+    "index.js": {
+      hash: async () => "11",
+      contents: async () => `const dynamic = import('./dynamic1')`,
+    },
+    dynamic1: {
+      hash: async () => "dead beef",
+      contents: async () => `import dynamic2 from "./static-from-dynamic"`,
+    },
+    "static-from-dynamic": {
+      hash: async () => "beef",
+      contents: async () => `tuff`,
     },
   },
 ]
